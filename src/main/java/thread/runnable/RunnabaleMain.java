dce5c5a16db4a9f9fcd51df040b6e11543e10c90
criso <criso>
2021-01-11 11:33:44 +0000

    implement thread with lambda help

--- src/main/java/thread/runnable/RunnabaleMain.java
+++ src/main/java/thread/runnable/RunnabaleMain.java
@@ -3,12 +3,7 @@ package thread.runnable;
 public class RunnabaleMain {
 
     public static void main(String[] args) {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Thread executed");
-            }
-        });
+        Thread t = new Thread(() -> System.out.println("Thread executed"));
         t.start();
     }
 }
